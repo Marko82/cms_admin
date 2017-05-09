9d64c0a96d3f916bff89fa5f30a8fa3d250453af
FreakONN <deus.commando@gmail.com>
2017-05-09 14:58:27 +0200

    initial commit

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class PostsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index($id)
+    {
+        //
+        return "The number is: " . $id;
+    }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return "I'm a method that creates!";
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        return "This is the show method with id parameter: " . $id;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    public function contact($id,$name,$password){
+        //vrlo loš način za prosljeđivanje podataka
+        //return view('contact')->with('id', $id);
+        //bolji način
+        return view('contact', compact('id','name','password'));
+    }
+
+     public function list(){
+        $data = [1,2,3];
+        return view('list',compact('data'));
+      }
+}
---
+++ app/Http/routes.php
@@ -0,0 +1,89 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+/*VAŽNO
+1)ovo je u višim verzijama Laravela maknuto u routes/ folder podijeljen u dva fajla api.php i web.php
+2)web.php sadrži routes s middlewareom i csrf-om
+*
+*/
+
+//'/' predstavlja korisnički definiranu rutu do view-a 'welcome' (http://localhost/cms_admin/public)
+//promjenom u '/example' i dalje dolazimo do 'welcome' view-a (http://localhost/cms_admin/public/example)
+
+/* Jednostavni primjeri
+Route::get('/', function () {
+    return view("welcome");
+
+});
+
+Route::get('/about', function () {
+    return "Hi about page!";
+
+});
+
+Route::get('/contact', function () {
+    return "Hi, I'm contact!";
+});
+*/
+
+//ruta do određenog posta - pr. 1 (http://localhost/cms_admin/public/post/1/marko)
+//unutar {} možemo staviti bilo koje ime za varijablu
+//da bi dohvatili varijablu moramo je staviti u function()
+
+Route::get('/post/{id}/{name}', function($id,$name){
+    //u pregledniku ispisuje prosljeđenu vrijednost u url-u  za $id i $name
+    return "This is post number: " . $id . " " . $name;
+});
+
+//primjer cusotm url-a s porukom
+//http://localhost/cms_admin/public/admin/posts
+
+Route::get('/admin/posts', function () {
+    return "Admin is here :D";
+
+});
+
+//možemo skratiti url (/admin/post/example) stavljajući ga u associative array
+Route::get('admin/posts/example', array('as' => 'admin.home', function(){
+
+    $url = route('admin.home');
+    return "this url is: " . $url;
+
+}));
+
+/*RESURSI -> php artisan route:list
+ * 1)sve između ' ' puput 'res' je url
+ * 2)napravili smo automatski url parametre za 'res'
+ * 3)napravili smo automatski url metode za 'res'
+ * 4)napravili smo automatski namespace za 'res'
+ */
+
+//pozivanje kontrolera - 'ImeKontrolera@njegovaMetoda'
+Route::get('/info/{id}','PostsController@index');
+
+//http://localhost:8000/res/1
+Route::resource('res','PostsController'); //This is the show method with id parameter: 1
+
+//prosljeđivanje with metodom
+//http://localhost:8000/contact/ada
+//http://localhost:8000/contact/3/marko/123 - za compact
+Route::get('/contact/{id}/{name}/{password}', 'PostsController@contact');
+
+//prikaz ktoz array-a
+Route::get('/admin',function (){
+    return view('admin', ['id' => 'Prvi']);
+});
+
+//prosljeđivanje ktoz array
+Route::get('/list', 'PostsController@list');
+
